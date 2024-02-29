9669b1575b196234dc7c92a870fdb72002177cf1
FengyunAlphabet <alphabet2varphi@foxmail.com>
2024-02-29 23:18:33 +0800

    okkk

--- app/src/main/assets/js/viewport.js
+++ app/src/main/assets/js/viewport.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidebar").style.height = "50px";
-    document.getElementById("main").style.marginTop = "50px";
+    document.getElementById("mySidebar").style.height = "40px";
+    document.getElementById("main").style.marginTop = "40px";
 }
 
 function closeNav() {
@@ -13,3 +13,41 @@ function delayedRedirect(url, delay) {
         window.location.href = url;
     }, delay);
 }
+
+function changepic() {
+    let pict = document.querySelector('img.sunANDmoon');
+    pict.style.opacity = 0;
+    setTimeout(function () {
+        if (pict.getAttribute('src') === "./img/sun.svg") {
+            pict.setAttribute('src', './img/moon.svg');
+            changeBODY_black();
+        }
+        else {
+            pict.setAttribute('src', './img/sun.svg');
+            changeBODY_ori();
+        }
+        pict.style.opacity = 1;
+    }, 300);
+}
+
+function changeBODY_black() {
+    document.body.style.backgroundColor = "black";
+    let imag = document.querySelector("img.Img");
+    imag.style.border = "2px solid wheat";
+}
+
+function changeBODY_ori() {
+    document.body.style.backgroundColor = "";
+    let imag = document.querySelector("img.Img");
+    imag.style.border = "2px solid wheat";
+}
+
+// function changepic2() {
+//     let pict = document.querySelector('.sunANDmoon');
+//     if (pict.style.backgroundPosition === 'left center, right center') {
+//         pict.style.backgroundPosition = 'right center, left center';
+//     }
+//     else {
+//         pict.style.backgroundPosition = 'left center, right center';
+//     }
+// }
